753844955cfbd50aab41b8af15edbc65bbc82fd6
qiqilele <qiqilelebaobao@163.com>
2025-05-09 15:02:32 +0800

    fix: version 1.0.45 update

--- easy_whitelist/__init__.py
+++ easy_whitelist/__init__.py
@@ -1,6 +1,6 @@
 r"""Easy_whitelist is a smart tool that detects the local Internet IP address and automatically updates the local Internet IP address to the cloud security group whitelist. The tool is written in Python.
 """
-__version__ = '1.0.44'
+__version__ = '1.0.45'
 # __author__ = 'qiqileleabaobao <qiqilelebaobao@163.com>'
 
 __all__ = []
