c27830881772cabb918bf9fed5f5c1d80bf2e1ef
qiqilelebaobao <qiqilelebaobao@163.com>
2025-09-17 18:10:02 +0800

    __main__.py refactor

--- easy_whitelist/__main__.py
+++ easy_whitelist/__main__.py
@@ -1,70 +1,96 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import logging
+import sys
+
 import json
 import os
 import pprint
 import string
-import sys
-import logging
 
 from easy_whitelist.config import arg
 from easy_whitelist.tcloud import client
 from easy_whitelist.tcloud.template import list_template, set_template, create_template
 
 
+LOG_FORMAT = '%(asctime)s-%(process)d-%(filename)s:%(lineno)d-%(levelname)s-%(message)s'
+INPUT_PROMPT = 'Please choose # template to set (or [L]ist, [C]reate, [Q]uit): '
+
 def loop_list(common_client, proxy=None):
     template_ids = list_template(common_client)
     last_input = None
-    while True:
-        input_from_user = input('Please choose # template to set (or [L]ist or [Q]uit): ')
-        if last_input == '' and input_from_user == '':
-            break
-        last_input = input_from_user
-        if input_from_user.isdigit():
-            if template_ids:
-                if (a := int(input_from_user)) > 0 and a <= len(template_ids):
-                    set_template(common_client, template_ids[a - 1], proxy)
-                else:
-                    logging.info('Wrong index, please input right index from the list.')
-        elif input_from_user == 'l' or input_from_user == 'L':
-            list_template(common_client)
-        elif input_from_user == 'q' or input_from_user == 'Q':
-            break
-        elif input_from_user == '':
-            continue
-        else:
-            logging.info('Input error.')
 
-def set_log(verbose=0):
-    # FMT = '%(asctime)s %(process)d %(filename)s L%(lineno)s %(levelname)s %(message)s'
-    FMT = '%(asctime)s - %(process)d - %(filename)s - L%(lineno)s - %(levelname)s - %(message)s'
-    
-    if verbose == 0:
-        logging.basicConfig(level=logging.WARN, format=FMT)
-    elif verbose == 1:
-        logging.basicConfig(level=logging.INFO, format=FMT)
-    elif verbose >=2:
-        logging.basicConfig(level=logging.DEBUG, format=FMT)
-    else:
-        print("Wrong position in set_log.")
-        
-def main():
+    while True:
+
+        try:
+            user_input = input(INPUT_PROMPT).strip().lower()
+
+            if user_input == 'q':
+                break
+
+            if last_input == '' and user_input == '':
+                break
+
+            last_input = user_input
+
+            if user_input == '':
+                continue
+
+            if user_input == 'l':
+                list_template(common_client)
+                continue
+
+            if user_input.isdigit():
+                if template_ids:
+                    if (a := int(user_input)) > 0 and a <= len(template_ids):
+                        set_template(common_client, template_ids[a - 1], proxy)
+                    else:
+                        logging.warning(
+                            'Index out of range. Available templates: %d', len(template_ids))
+                    continue
+
+            logging.warning('Invalid input: %s', user_input)
+
+        except KeyboardInterrupt:
+            logging.warning('\nOperation cancelled by user')
+            break
+        except Exception as e:
+            logging.warning("\nUnexpected error occurred: %s", e)
+
+
+def set_log(verbose: int = 0):
+
+    level_map = {
+        0: logging.WARNING,
+        1: logging.INFO,
+        2: logging.DEBUG
+    }
+
+    level = level_map.get(verbose, logging.WARNING)
+
+    logging.basicConfig(level=level, format=LOG_FORMAT)
+
+
+def main() -> None:
     tencent, alibaba, action, target, target_id, proxy, verbose = arg.init_arg()
 
     set_log(verbose)
 
     common_client = client.get_common_client(proxy)
-    
-    if tencent and target == 'template':
+
+    cloud_provider = 'tencent' if tencent else 'aliyun'
+    logging.info('Using %s cloud provider', cloud_provider.upper())
+
+    if cloud_provider == 'tencent' and target == 'template':
         if action == 'list':
-            loop_list(common_client)
+            loop_list(common_client, proxy)
         elif action == 'set':
             set_template(common_client, target_id, proxy)
         elif action == 'create':
             create_template(common_client, target_id, proxy)
         else:
-            logging.error('Wrong postion, shall not be here.')
+            logging.error('Unsupported operation: %s', action)
 
 
 if __name__ == '__main__':
