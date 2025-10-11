3686e07486dea4ff9988b6232dc097ad9c52e1bd
qiqilelebaobao <qiqilelebaobao@163.com>
2025-10-11 14:06:07 +0800

    for setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="easy_whitelist",              # PyPI 上唯一的名字
-    version="1.0.92",                   # 每次上传必须 > 旧版本
+    version="1.0.96",                   # 每次上传必须 > 旧版本
     author="qiqilelebaobao",
     author_email="qiqilelebaobao@163.com",
     description="A smart tool that detects the local Internet IP address and automatically updates the local Internet IP address to the cloud security group whitelist.",
@@ -13,6 +13,7 @@ setup(
     long_description_content_type="text/markdown",
     keywords=["whitelist", "security-groups", "alibaba-cloud", "tencent-cloud", "security-tools"],
     url="https://github.com/qiqilelebaobao/easy_whitelist",
+    license="Apache License 2.0",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -27,11 +28,28 @@ setup(
         "Topic :: System :: Networking",
         "Intended Audience :: System Administrators"
     ],
+    package_data={
+        "easy_whitelist": ["*.txt"],   # 键=包名，值=glob 列表
+    },
+    setup_requires=[
+        "setuptools>=61.0",
+        "wheel",
+        "Cython"
+    ],
+    install_requires=[
+        "requests",
+        "tencentcloud-sdk-python"
+    ],
+    entry_points={
+        "console_scripts": [
+            "ew=easy_whitelist._core:main",
+        ],
+    },
     extras_require={
         "cli": [
             "rich",
             "click>=5.0",
         ],
     },
-    python_requires=">=3.8",
+    python_requires=">=3.8"
 )
